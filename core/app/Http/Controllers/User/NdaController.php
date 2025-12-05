b4a669b57be7b7f60bf09a0f05bc8ae4d2f2e2e8
Dexterity Wurayayi <dexterity.wurayayi@qatctech.com>
2025-12-05 12:44:28 +0200

    fix nda

--- core/app/Http/Controllers/User/NdaController.php
+++ core/app/Http/Controllers/User/NdaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Constants\Status;
+use App\Http\Controllers\Controller;
 use App\Models\Listing;
 use App\Models\NdaDocument;
 use Illuminate\Http\Request;
